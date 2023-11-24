6820296f6ada77d2ec988d7525c5ac11b352d486
Alan Li <lizengqiang1997@gmail.com>
2023-11-24 23:57:08 +0800

    iBeacon init

--- IBeacon/ViewController.swift
+++ IBeacon/ViewController.swift
@@ -6,14 +6,129 @@
 //
 
 import UIKit
+import CoreLocation
+import SnapKit
 
 class ViewController: UIViewController {
+    let locationManager = CLLocationManager()
 
+    let distance = UILabel()
+    let rssi = UILabel()
+    let major = UILabel()
+    let minor = UILabel()
+    let container =  UIStackView()
+   let uuid = UUID.init(uuidString: "FDA50693-A4E2-4FB1-AFCF-C6EB07647825")!
     override func viewDidLoad() {
         super.viewDidLoad()
+        initView()
+    
+       
         // Do any additional setup after loading the view.
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        
+//        let region = CLBeaconRegion(uuid:uuid,major: CLBeaconMajorValue(01),minor: CLBeaconMinorValue(02),identifier: "iBeacon Region");
+//            
+//                                    
+//        locationManager.startMonitoring(for:region)
+        print("搜索中")
+        
+        locationManager.startRangingBeacons(satisfying: CLBeaconIdentityConstraint(uuid:uuid,major: CLBeaconMajorValue(UInt16(01)),minor: CLBeaconMinorValue(UInt16(02))))
     }
 
+    
+    func initView(){
+        container.axis = .vertical
+        container.alignment = .top
+        container.addArrangedSubview(distance)
+        container.addArrangedSubview(rssi)
+        container.addArrangedSubview(major)
+        container.addArrangedSubview(minor)
+        self.view.addSubview(container)
+        
+        container.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.left.right.equalToSuperview().offset(10)
+        
+        }
+      
+        distance.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(container.snp.top).offset(10)
+            make.height.equalTo(40)
+        }
+        
+        rssi.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(distance.snp.bottom).offset(10)
+            make.height.equalTo(40)
+        }
+        
+        major.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(rssi.snp.bottom).offset(10)
+            make.height.equalTo(40)
+        }
+        
+        minor.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(major.snp.bottom).offset(10)
+            make.height.equalTo(40)
+        }
+      
+    }
 
 }
+
+extension ViewController :CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location Manager did fail: \(error.localizedDescription)")
+    }
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
+        print("Location Manager monitoring did fail:\(error.localizedDescription)")
+    }
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        print("didStartMonitoringFor")
+        // 開始偵測範圍之後，就先檢查目前的 state 是否在範圍內
+        manager.requestState(for: region)
+    }
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("didEnterRegion")
+        guard region is CLBeaconRegion else {return}
+        guard CLLocationManager.isRangingAvailable() else{return}
+        manager.startRangingBeacons(satisfying: CLBeaconIdentityConstraint(uuid:uuid,major: CLBeaconMajorValue(UInt16(01)),minor: CLBeaconMinorValue(UInt16(02))))
+    
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("didExitRegion")
+        guard region is CLBeaconRegion else {return}
+        guard CLLocationManager.isRangingAvailable() else {return}
+        manager.stopRangingBeacons(satisfying: CLBeaconIdentityConstraint(uuid:uuid,major: CLBeaconMajorValue(UInt16(01)),minor: CLBeaconMinorValue(UInt16(02))))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, rangingBeaconsDidFailFor region: CLBeaconRegion, withError error: Error) {
+        print("Location manager ranging beacons did fail: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        print("didRangeBeacons\(beacons.count)")
+        if let beacon = beacons.first{
+            print("距离最近的beacon : \(beacon.`self`())")
+            distance.text = "IBeacon 信标的距离是：\(rssi2Distance(rssi: beacon.rssi))"
+            major.text = "major:\(beacon.major)"
+            minor.text = "minor:\(beacon.minor)"
+            rssi.text = "rssi:\(beacon.rssi)"
+        }
+    }
+    
+    func rssi2Distance(rssi: Int) -> Double {
+        let iRssi = abs(rssi)
+          // 发射端和接收端 相隔1m的信号强度
+        let s :Double = 60.0
+          // 环境衰减因子
+          let f = 5.67
+        let power :Double = ( Double(iRssi) - s) / (10.0 * f)
+        return pow(10.0,power)
+      }
+
+}
+
 
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+
+target 'IBeacon' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for IBeacon
+  # SnapKit
+    pod 'SnapKit', '~> 5.6.0'
+end
+
